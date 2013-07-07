3638f073e261869ef1d2f1989731d52229998f60
unknown <Raymond@.(none)>
2013-07-06 18:08:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MangaNotifier::Application.config.secret_token = 'ef2b7338f879aad9ad92921409b7bfd5854a85c641cb922db24f3576ff895e1230ee204792a7b0a6e54f3bf164578a26f02fbd8e9f9c1abfbc3028907c0f5497'
